90247597ece6d7fa8ee6377959b948b11c860dbb
jeremyt135 <jdt13579@gmail.com>
2020-01-01 13:11:35 -0800

    Add database config

---
+++ database/db.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require('mongodb')
+
+exports.connectToDb = async config => {
+  const client = await MongoClient.connect(config.url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  return { client, db: client.db(config.name) }
+}
