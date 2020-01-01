ccbb8c73fba83c6b5634d1fd0b8e4396b6a6140b
jeremyt135 <jdt13579@gmail.com>
2020-01-01 15:01:59 -0800

    Add authorization middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = async (req, res, next) => {
+  const token = req.header('x-auth-token')
+
+  if (!token) {
+    return res.status(401).json({ msg: 'Authorization denied' })
+  }
+
+  try {
+    await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) {
+        res.status(401).json({ msg: 'Authorization denied' })
+      } else {
+        req.user = decoded.user
+        next()
+      }
+    })
+  } catch (error) {
+    console.error('Error in JWT authorization middleware\n', error)
+    res.status(500).json({ msg: 'Internal server error' })
+  }
+}
