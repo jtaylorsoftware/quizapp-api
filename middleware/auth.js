ff912122926c1a6bf572c6c7b6358003464d1c87
jeremyt135 <jdt13579@gmail.com>
2020-01-01 15:15:54 -0800

    Change how error json responses are sent

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,15 @@ module.exports = async (req, res, next) => {
   const token = req.header('x-auth-token')
 
   if (!token) {
-    return res.status(401).json({ msg: 'Authorization denied' })
+    return res.status(401).json({ errors: [{ msg: 'Authorization denied' }] })
   }
 
   try {
     await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
       if (error) {
-        res.status(401).json({ msg: 'Authorization denied' })
+        return res
+          .status(401)
+          .json({ errors: [{ msg: 'Authorization denied' }] })
       } else {
         req.user = decoded.user
         next()
@@ -18,6 +20,6 @@ module.exports = async (req, res, next) => {
     })
   } catch (error) {
     console.error('Error in JWT authorization middleware\n', error.message)
-    res.status(500).json({ msg: 'Internal server error' })
+    res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
   }
 }
