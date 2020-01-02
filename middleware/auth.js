22a3c18879abd1566c97dff8b3c6a9bac99df9bf
jeremyt135 <jdt13579@gmail.com>
2020-01-02 12:34:42 -0800

    Change authenticate middleware to take options
    Authenticate middleware function now takes an options object (currently required is only option). If options.required is set, the middleware will set res to status 401 if authentication fails.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,25 @@
 const jwt = require('jsonwebtoken')
 
-module.exports = async (req, res, next) => {
+const verifyToken = async token => {
+  let user = null
+  await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (!error) {
+      user = decoded.user
+    }
+  })
+  return user
+}
+
+exports.authenticate = options => async (req, res, next) => {
   const token = req.header('x-auth-token')
-
-  if (!token) {
-    return res.status(401).json({ errors: [{ msg: 'Authorization denied' }] })
-  }
-
   try {
-    await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-      if (error) {
-        return res
-          .status(401)
-          .json({ errors: [{ msg: 'Authorization denied' }] })
-      } else {
-        req.user = decoded.user
-        next()
-      }
-    })
+    const user = await verifyToken(token)
+    if (!user && options.required) {
+      res.status(401).json({ errors: [{ msg: 'Authorization denied' }] })
+    } else {
+      req.user = user
+      next()
+    }
   } catch (error) {
     console.error('Error in JWT authorization middleware\n', error.message)
     res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
