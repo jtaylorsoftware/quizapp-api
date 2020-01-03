998caa43b320aa9a781dd1d1065a3e304a92aabf
jeremyt135 <jdt13579@gmail.com>
2020-01-03 14:43:27 -0800

    Add repository base class with basic operations

---
+++ repositories/repository.js
@@ -0,0 +1,37 @@
+const { ObjectId } = require('mongodb')
+
+exports.Repository = class Repository {
+  constructor(store) {
+    this.store = store
+  }
+
+  /**
+   * Adds an entity to the repository
+   * @param {Object} doc entity document
+   * @returns {Object} Inserted document
+   */
+  async insert(quiz) {
+    const { ops } = await this.store.insertOne(quiz)
+    return ops[0]
+  }
+
+  /**
+   * Finds a single entity by id
+   * @param {String} id
+   * @returns {Object} Quiz data
+   */
+  async findById(id) {
+    if (!ObjectId.isValid(id)) {
+      return null
+    }
+    return await this.store.findOne({ _id: new ObjectId(id) })
+  }
+
+  /**
+   * Deletes a single entity by id
+   * @param {Object} ID
+   */
+  async delete(id) {
+    await this.store.deleteOne({ _id: id })
+  }
+}
