f3b892ab1c7d3a5a16f5d6276240a660d9ce7a80
jeremyt135 <jdt13579@gmail.com>
2020-01-03 14:01:21 -0800

    Add method for removing a quiz from user's quizzes

--- repositories/user.js
+++ repositories/user.js
@@ -21,20 +21,45 @@ exports.UserRepository = class UserRepository {
   }
 
   /**
-   * Updates a user in the repository
+   * Updates a user's quizzes
    * @param {User|string|ObjectId} user User or ID of User to modify
-   * @param {Object} quizId ObjectId of a Quiz to add
+   * @param {Quiz|string|ObjectId} quiz Quiz or ID a Quiz to add
    * @returns {Object} User data without sensitive information
    */
-  async addQuiz(user, quizId) {
+  async addQuiz(user, quiz) {
     const _id = user._id || (ObjectId.isValid(user) ? new ObjectId(user) : '')
-    if (!_id) {
+    const quizId =
+      quiz._id || (ObjectId.isValid(quiz) ? new ObjectId(quiz) : '')
+    if (!_id || !quizId) {
       return null
     }
     await this.store.updateOne(
       { _id },
       {
         $addToSet: {
+          quizzes: quizId
+        }
+      }
+    )
+  }
+
+  /**
+   * Remove a quiz from User's quizzes
+   * @param {User|string|ObjectId} user User or ID of User to modify
+   * @param {Quiz|string|ObjectId} quiz Quiz or ID a Quiz to remove
+   * @returns {Object} User data without sensitive information
+   */
+  async removeQuiz(user, quiz) {
+    const _id = user._id || (ObjectId.isValid(user) ? new ObjectId(user) : '')
+    const quizId =
+      quiz._id || (ObjectId.isValid(quiz) ? new ObjectId(quiz) : '')
+    if (!_id || !quizId) {
+      return null
+    }
+    await this.store.updateOne(
+      { _id },
+      {
+        $pull: {
           quizzes: quizId
         }
       }
