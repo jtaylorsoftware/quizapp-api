8a875fd8c678452238e8a207a5b8edaefa4cc346
jeremyt135 <jdt13579@gmail.com>
2020-01-03 13:26:12 -0800

    Modify existing routes to use changes and add PUT routes
    Existing routes now use the most recent Quiz model and make use of validation functions through the middleware file middleware/validation/quiz.js. Also added PUT routes for modifying quizzes

--- routes/api/quiz.js
+++ routes/api/quiz.js
@@ -1,4 +1,4 @@
-const { check } = require('express-validator')
+const validators = require('../../middleware/validation/quiz')
 const { checkErrors } = require('../../middleware/checkerrors')
 const { UserRepository } = require('../../repositories/user')
 const { QuizRepository } = require('../../repositories/quiz')
@@ -11,38 +11,36 @@ const omit = require('object.omit')
 let userRepository
 let quizRepository
 
-const checkQuestionText = check('text', 'Question must have a non-empty text')
-  .isString()
-  .isLength({ min: 1 })
-const checkPublic = check('isPublic', 'Must set a value for public').isBoolean()
-const checkExpiresIn = check(
-  'expiresIn',
-  'expiresIn must be an integer >= 0'
-).isInt({ min: 0 })
-const checkUser = check(
-  'user',
-  'User creating quiz must be null or a user ID'
-).custom(value => value == null || QuizRepository.validateUser(value))
-const checkAllowedUsers = check(
-  'allowedUsers',
-  'Allowed users must be null or empty or only contain user IDs'
-).custom(
-  value =>
-    value == null ||
-    (value instanceof Array && value.length === 0) ||
-    QuizRepository.validateAllowedUsers(value)
-)
-const checkQuestions = check(
-  'questions',
-  'Questions must be a valid array of question objects'
-).custom(value => QuizRepository.validateQuestions(value))
+const getRequestedQuiz = async (req, res, next) => {
+  try {
+    const quiz = await quizRepository.findById(req.params.id)
+    if (!quiz) {
+      return res
+        .status(404)
+        .json({ errors: [{ msg: 'There is no matching quiz found' }] })
+    }
+    req.quiz = quiz
+    next()
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
+  }
+}
+
+const canViewQuiz = (id, quiz) =>
+  quiz.isPublic ||
+  quiz.user.toString() === id ||
+  !quiz.allowedUsers.some(userId => userId.toString() === id)
 
 const router = require('express').Router()
 
+/**
+ * GET /
+ * Returns all public quizzes
+ */
 router.get('/', async (req, res) => {
   try {
     const quizzes = await quizRepository.getAllPublicQuizzes()
-    console.log(quizzes)
     res.json(quizzes)
   } catch (error) {
     console.error(error)
@@ -50,33 +48,193 @@ router.get('/', async (req, res) => {
   }
 })
 
+/**
+ * GET /:id
+ * Returns a public quiz by id or a private one if user is authenticated & allowed to
+ */
+router.get(
+  '/:id',
+  authenticate({ required: false }),
+  getRequestedQuiz,
+  async (req, res) => {
+    const { quiz, user } = req
+    if (user) {
+      if (!canViewQuiz(user.id, quiz)) {
+        return res.status(403).json({
+          errors: [{ msg: 'You are not allowed to view this quiz' }]
+        })
+      }
+    } else if (!quiz.isPublic) {
+      return res.status(401).json({
+        errors: [{ msg: 'You must be logged in to view this quiz' }]
+      })
+    }
+    res.json(req.quiz)
+  }
+)
+
+/**
+ * POST /
+ * Saves a quiz
+ */
 router.post(
   '/',
+  authenticate({ required: false }),
   [
-    checkQuestionText,
-    checkPublic,
-    checkExpiresIn,
-    checkUser,
-    checkAllowedUsers,
-    checkQuestions
+    validators.checkQuizTitle,
+    validators.checkPublic,
+    validators.checkExpiresIn,
+    validators.checkAllowedUsers,
+    validators.checkQuestions
   ],
   checkErrors,
-  authenticate({ required: false }),
   async (req, res) => {
-    const {
-      user,
-      text,
-      allowedUsers,
-      questions,
-      isPublic,
-      expiresIn
-    } = req.body
-
+    const user = req.user.id || null // if undefined, will set to null
+    const { title, allowedUsers, questions, isPublic } = req.body
+    const expiresIn = Number.parseInt(req.body.expiresIn)
     try {
-      await quizRepository.insert(
-        new Quiz(user, text, questions, allowedUsers, expiresIn, isPublic)
+      const quiz = await quizRepository.insert(
+        new Quiz(user, title, questions, allowedUsers, expiresIn, isPublic)
       )
-      res.status(200).end()
+      res.status(200).json({ id: quiz._id })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
+    }
+  }
+)
+
+/**
+ * DELETE /:id
+ * Deletes a quiz by id if authenticated and owns the quiz
+ */
+router.delete(
+  '/:id',
+  authenticate({ required: true }),
+  getRequestedQuiz,
+  validators.requireQuizOwner,
+  async (req, res) => {
+    const { quiz } = req
+    try {
+      await quizRepository.delete(quiz)
+      res.status(204).end()
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
+    }
+  }
+)
+
+/**
+ * PUT /:id/title
+ * Changes the quiz title
+ */
+router.put(
+  '/:id/title',
+  authenticate({ required: true }),
+  getRequestedQuiz,
+  validators.requireQuizOwner,
+  [validators.checkQuizTitle],
+  checkErrors,
+  async (req, res) => {
+    const { quiz } = req
+    const { title } = req.body
+    try {
+      await quizRepository.updateTitle(quiz, title)
+      res.status(204).end()
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
+    }
+  }
+)
+
+/**
+ * PUT /:id/questions/:questionIndex/text
+ * Change the text for a question
+ */
+router.put(
+  '/:id/questions/:questionIndex/text',
+  authenticate({ required: true }),
+  getRequestedQuiz,
+  validators.requireQuizOwner,
+  [validators.checkQuestionIndex, validators.checkQuestionText],
+  checkErrors,
+  async (req, res) => {
+    const { text } = req.body
+    const { quiz } = req
+    const questionIndex = Number.parseInt(req.params.questionIndex)
+    if (questionIndex >= quiz.questions.length) {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'questionIndex out of bounds',
+            location: 'params',
+            param: 'questionIndex'
+          }
+        ]
+      })
+    }
+    try {
+      await quizRepository.updateQuestionText(quiz, questionIndex, text)
+      res.status(204).end()
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
+    }
+  }
+)
+
+/**
+ * PUT /:id/questions/:questionIndex/answers/:answerIndex/text
+ * Change the text for an answer to a question
+ */
+router.put(
+  '/:id/questions/:questionIndex/answers/:answerIndex/text',
+  authenticate({ required: true }),
+  getRequestedQuiz,
+  validators.requireQuizOwner,
+  [
+    validators.checkQuestionIndex,
+    validators.checkAnswerIndex,
+    validators.checkAnswerText
+  ],
+  checkErrors,
+  async (req, res) => {
+    const { text } = req.body
+    const { quiz } = req
+    const questionIndex = Number.parseInt(req.params.questionIndex)
+    const answerIndex = Number.parseInt(req.params.answerIndex)
+    if (questionIndex >= quiz.questions.length) {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'questionIndex out of bounds',
+            location: 'params',
+            param: 'questionIndex'
+          }
+        ]
+      })
+    }
+    if (answerIndex >= quiz.questions[questionIndex].answers.length) {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'answerIndex out of bounds',
+            location: 'params',
+            param: 'questionIndex'
+          }
+        ]
+      })
+    }
+    try {
+      await quizRepository.updateQuestionAnswerText(
+        quiz,
+        questionIndex,
+        answerIndex,
+        text
+      )
+      res.status(204).end()
     } catch (error) {
       console.error(error)
       res.status(500).json({ errors: [{ msg: 'Internal server error' }] })
