9e12bbe0a8af66172d000ea7f71c6b8ad96f8787
jeremyt135 <jdt13579@gmail.com>
2020-01-03 15:32:20 -0800

    Change model parameters and constructor

--- models/quiz.js
+++ models/quiz.js
@@ -1,12 +1,19 @@
 const { ObjectId } = require('mongodb')
 
 exports.Quiz = class Quiz {
-  constructor(user, title, questions, allowedUsers, expiresIn, isPublic) {
+  constructor(
+    user,
+    title,
+    expiresIn,
+    isPublic = true,
+    questions = [],
+    allowedUsers = []
+  ) {
     this.user = ObjectId.isValid(user) ? new ObjectId(user) : null
     this.title = title
     this.allowedUsers = allowedUsers
     this.questions = questions
-    this.responses = 0
+    this.completeResponses = []
     this.isPublic = isPublic
     this.expiresIn = expiresIn
     this.date = new Date().toISOString()
