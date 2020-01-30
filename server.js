7de5c8a87ea91d2d7e70bf290a185e8485ab01d0
jeremyt135 <jdt13579@gmail.com>
2020-01-30 09:26:59 -0800

    Fix missing require for 'path'

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const debug = require('debug')('server')
 const express = require('express')
-
+const path = require('path')
 const routes = require('./routes')
 
 const { connectToDb } = require('./database/db')
