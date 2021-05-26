f6d34e6f736da50f8371f7a2a7ddab3835ea7d91
jeremyt135 <jdt13579@gmail.com>
2021-05-26 15:31:06 -0700

    Add default port and catch-all for unhandled rejection

--- src/start.ts
+++ src/start.ts
@@ -7,19 +7,25 @@ import path from 'path'
 
 import bootstrap from './bootstrap-app'
 
-bootstrap().then(({ app, client, config }) => {
-  const PORT = Number.parseInt(process.env.PORT)
-  const name =
-    (config?.name && 'Application "' + config.name + '"') || 'Application'
+bootstrap()
+  .then(({ app, client, config }) => {
+    const DEFAULT_PORT = 8080
+    let port = Number.parseInt(process.env.PORT)
+    port = port == NaN ? DEFAULT_PORT : port
+    const name =
+      (config?.name && 'Application "' + config.name + '"') || 'Application'
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static('client/build'))
+      app.get('*', (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname, '../client', 'build', 'index.html')
+        )
+      })
+    }
+
+    app.listen(port, () => {
+      console.log(`${name} started on port ${port}`)
     })
-  }
-
-  app.listen(PORT, () => {
-    console.log(`${name} started on port ${PORT}`)
   })
-})
+  .catch((err) => console.error('Error starting server:\n', err))
