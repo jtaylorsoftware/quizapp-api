7cf90ba31edbb40dabd664eae65dc2caa778af7d
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2022-03-13 17:26:23 -0700

    Fix signal handling

--- src/start.ts
+++ src/start.ts
@@ -1,37 +1,39 @@
-import * as http from 'http'
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-import express from 'express'
-import path from 'path'
+import { createServer } from 'http'
 import { createTerminus, TerminusOptions } from '@godaddy/terminus'
 
 import bootstrap from './bootstrap-app'
 
-async function onSignal () {
-  console.log('server is starting cleanup')
-}
-
-async function onShutdown () {
-  console.log('cleanup finished, server is shutting down');
-}
-
-async function onHealthCheck () {
-  return 'OK'
-}
-
 bootstrap()
-  .then(({ app, client, config }) => {
-    const DEFAULT_PORT = 8080
-    let port = Number.parseInt(process.env.PORT)
-    port = Number.isNaN(port) ? DEFAULT_PORT : port
-    const name =
-      (config?.name && 'Application "' + config.name + '"') || 'Application'
+  .then(({ app, client: mongoClient, config }) => {
+    // Express app should now be configured, routes set up, and connected to MongoDB
 
-    const server = http.createServer(app)
+    // Get port from env or default
+    let port = (() => {
+      const DEFAULT_PORT = 8080
+      const envPort = Number.parseInt(process.env.PORT)
+      return Number.isNaN(envPort) ? DEFAULT_PORT : envPort
+    })()
 
+    // Callbacks for terminus
+    async function onSignal() {
+      console.log('server received shutdown signal')
+      await mongoClient.close()
+    }
+
+    async function onShutdown() {
+      console.log('server is shutting down')
+      return Promise.resolve()
+    }
+
+    async function onHealthCheck() {
+      return 'OK'
+    }
+
+    // Set up healthcheck endpoint and signal handlers
     const options: Partial<TerminusOptions> = {
       healthChecks: {
         '/healthcheck': onHealthCheck,
@@ -39,13 +41,14 @@ bootstrap()
       },
       signals: ['SIGINT', 'SIGTERM'],
       onSignal,
-      onShutdown
+      onShutdown,
     }
-
+    const server = createServer(app)
     createTerminus(server, options)
 
+    // Start serving requests
     server.listen(port, () => {
-      console.log(`${name} started on port ${port}`)
+      console.log(`Application${config?.name ? ' ' + config?.name : ''} started on port ${port}`)
     })
   })
   .catch((err) => console.error('Error starting server:\n', err))
