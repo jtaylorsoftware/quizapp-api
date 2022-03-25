c28136c5a66cb9151e554c478ce37136a7a6f316
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2022-03-24 19:30:48 -0700

    Integrate swagger-ui behind config value, default disabled in prod

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import { MongoClient } from 'mongodb'
 
-import { Inject, Application } from 'express-di'
+import { Application, Inject, OpenApiConfig } from 'express-di'
 import DbService from 'services/db'
+import path from 'path'
+
+const openApiOptions: OpenApiConfig | undefined = process.env.NODE_ENV !== 'production' ?
+  {
+    path: '/openapi',
+    documentPath: path.resolve('openapi', 'swagger.yaml'),
+  } : undefined
 
 @Inject
 export class App extends Application({
@@ -10,10 +17,12 @@ export class App extends Application({
     contentSecurityPolicy: false,
   },
   corsConfig: {
-    origin: process.env.NODE_ENV == 'production' ? 'http://www.makequizzes.online' : '*'
-  }
+    origin: process.env.NODE_ENV == 'production' ? 'http://www.makequizzes.online' : '*',
+  },
+  openApi: openApiOptions,
 }) {
   dbClient!: MongoClient
+
   constructor(private db: DbService) {
     super()
   }
--- src/express-di/application.ts
+++ src/express-di/application.ts
@@ -3,11 +3,20 @@ import helmet, { HelmetOptions } from 'helmet'
 import { DisableInjection } from './injection'
 import { RequestHandler, Route } from './route'
 import cors, { CorsOptions, CorsOptionsDelegate } from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yaml'
+import fs from 'fs'
+
+export type OpenApiConfig = {
+  path: string
+  documentPath: string
+}
 
 export interface ApplicationConfig {
   name?: string
   helmetOptions?: Readonly<HelmetOptions>
   corsConfig?: CorsOptions | CorsOptionsDelegate
+  openApi?: OpenApiConfig
 }
 
 export interface ApplicationBase {
@@ -24,6 +33,15 @@ export default function Application(config?: ApplicationConfig) {
       this.ex.use(helmet(config?.helmetOptions))
       this.ex.use(cors(config?.corsConfig))
       this.ex.use(express.json())
+
+      if (config?.openApi != null) {
+        try {
+          const document = YAML.parse(fs.readFileSync(config.openApi.documentPath, 'utf8'))
+          this.ex.use(config.openApi.path, swaggerUi.serve, swaggerUi.setup(document))
+        } catch (err) {
+          console.error(`Could not open OpenAPI document ${config.openApi.documentPath}: `, err)
+        }
+      }
 
       // @ts-ignore
       this.ex.use((error, req, res, next) => {
