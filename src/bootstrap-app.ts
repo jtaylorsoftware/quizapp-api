5ce8028385634d4daeff8e26bc248c7a2239c362
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2022-04-30 19:10:40 -0700

    Remove api/v1
    - All (of my) frontends no longer rely on v1 and it is now
      safe for removal

--- src/bootstrap-app.ts
+++ src/bootstrap-app.ts
@@ -4,10 +4,6 @@ import { App } from 'app'
 
 import DbService from 'services/db'
 
-import QuizControllerV1 from 'controllers/v1/quiz'
-import ResultControllerV1 from 'controllers/v1/result'
-import UserControllerV1 from 'controllers/v1/user'
-
 import QuizControllerV2 from 'controllers/v2/quiz'
 import ResultControllerV2 from 'controllers/v2/result'
 import UserControllerV2 from 'controllers/v2/user'
@@ -15,10 +11,6 @@ import UserControllerV2 from 'controllers/v2/user'
 import QuizRepository from 'repositories/quiz'
 import ResultRepository from 'repositories/result'
 import UserRepository from 'repositories/user'
-
-import QuizServiceV1 from 'services/v1/quiz'
-import ResultServiceV1 from 'services/v1/result'
-import UserServiceV1 from 'services/v1/user'
 
 import QuizServiceV2 from 'services/v2/quiz'
 import ResultServiceV2 from 'services/v2/result'
@@ -29,25 +21,13 @@ export default async function () {
   const app = <App>(
     await bootstrap(
       App,
-      [
-        QuizControllerV1,
-        ResultControllerV1,
-        UserControllerV1,
-
-        QuizControllerV2,
-        ResultControllerV2,
-        UserControllerV2,
-      ],
+      [QuizControllerV2, ResultControllerV2, UserControllerV2],
       [
         DbService,
 
         UserRepository,
         QuizRepository,
         ResultRepository,
-
-        UserServiceV1,
-        QuizServiceV1,
-        ResultServiceV1,
 
         UserServiceV2,
         QuizServiceV2,
